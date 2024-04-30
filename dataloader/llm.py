fix: batch metrics calculation to avoid oom errors

dataloader/llm.py
@@ -8,7 +8,7 @@ import torch.utils.data as data_utils
 from transformers import AutoTokenizer
 from transformers.models.llama.tokenization_llama import DEFAULT_SYSTEM_PROMPT
 
-from trainer import absolute_recall_mrr_ndcg_for_ks
+from trainer import absolute_metrics_batch_wrapper
 
 from .utils import Prompter
 
@@ -149,14 +149,14 @@ class LLMDataloader:
             "original_size": len(self.test_probs),
             "retrieval_size": len(self.test_candidates),
             "original_metrics": retrieved_file["test_metrics"],
-            "retrieval_metrics": absolute_recall_mrr_ndcg_for_ks(
+            "retrieval_metrics": absolute_metrics_batch_wrapper(
                 torch.tensor(self.test_probs)[torch.tensor(self.test_users) - 1],
                 torch.tensor(self.test_labels)[torch.tensor(self.test_users) - 1],
                 args.metric_ks,
                 num_classes=self.item_count + 1,
                 preprocessed=True,
             ),
-            "non_retrieval_metrics": absolute_recall_mrr_ndcg_for_ks(
+            "non_retrieval_metrics": absolute_metrics_batch_wrapper(
                 torch.tensor(self.test_probs)[torch.tensor(self.non_test_users) - 1],
                 torch.tensor(self.test_labels)[torch.tensor(self.non_test_users) - 1],
                 args.metric_ks,